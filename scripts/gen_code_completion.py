c3fb64d4eed12b5eeb2ac3fc0ccba9490ed39965
Luca Z <l-zeuch@email.de>
2022-03-01 22:52:59 +0000

    Update gen_code_completion.py

--- scripts/gen_code_completion.py
+++ scripts/gen_code_completion.py
@@ -1,8 +1,8 @@
 # Python script to generate code completion sources for use with
 # https://github.com/hrsh7th/nvim-cmp. Outputs lua code.
-
+#
 # Copyright (C) 2022    Lucas Ritzdorf, Luca Zeuch
-
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -11,7 +11,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
